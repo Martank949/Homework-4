6b70a36a8193312da150101271694547709abef7
Gargantua <martinzb949@gmail.com>
2020-10-19 20:38:48 -0700

    scrapped OG plan and re-doing the structure, stated my variables and created the divs

--- apps.js
+++ apps.js
@@ -1,29 +1,3 @@
-        //this is a button that should start or cancel the start timer or test
-        document.getElementById("startButton").onclick = function() {
-            //document.getElementById("startButton").style.display = "none";
-            var txt;
-            //Pop up that allows user to choose ok or cancel
-            var button = confirm("You have 2 minutes to complete this test, press 'OK' to start");
-            //if user clicks "ok"
-            if (button == true) {
-                var timeLeft = 10;
-                document.getElementById("startButton").style.display = "none";
-                var timer = setInterval(function() {
-                    if (timeLeft == 0) {
-                        clearInterval(timer);
-
-                        //timer is up
-                        document.getElementById("demo").innerHTML = "Put your keyboard fingers up!";
-                        alert("Time is Up!");
-                    } else {
-                        document.getElementById("demo").innerHTML = timeLeft + " Seconds Remaining";
-                        //progress bar
-                        document.getElementById("progressBar").value = 10 - timeLeft;
-                    }
-                    timeLeft -= 1;
-                }, 1000);
-
-            } else {
-                alert("Its ok if you are not ready, hit the books and come back when you are ready!");
-            }
-        }
+var locationInTest = 0;
+var correct = 0;
+var choice, choices, choiceA, choiceB, choiceC, test, testLocator, question, quiz;
