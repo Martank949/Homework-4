bb1784a5c22be4d857d34cbe993eb2facc3c2d12
Gargantua <martinzb949@gmail.com>
2020-10-21 19:54:07 -0700

    added a new timer function for the test, and removed some un used styling classes

--- apps.js
+++ apps.js
@@ -1,6 +1,31 @@
        var locationInTest = 0;
        var correct = 0;
        var choice, choices, choiceA, choiceB, choiceC, test, testLocator, question, quiz;
+
+       document.getElementById("startButton").onclick = function strBtn() {
+           var txt;
+           var button = confirm("You have 2 minutes to complete this test, press 'OK' to start");
+           //if user clicks "ok"
+           if (button == true) {
+               var timeLeft = 30;
+               var timer = setInterval(function strTmr() {
+                   if (timeLeft <= 0) {
+                       clearInterval(timer);
+                       //timer is up
+                       document.getElementById("demo").innerHTML = "Put your keyboard fingers up!";
+                   } else {
+                       document.getElementById("demo").innerHTML = timeLeft + " Seconds Remaining";
+                       //progress bar
+                       document.getElementById("progressBar").value = 30 - timeLeft;
+                   }
+                   timeLeft -= 1;
+               }, 1000);
+
+           } else {
+               alert("Its ok if you are not ready, hit the books and come back when you are ready!");
+           }
+       }
+
        var questions = [{
            question: "Select the correct statement:",
            a: "JavaScript is a server-side only language",
